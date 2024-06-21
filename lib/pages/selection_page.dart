163df43b176efb615221be3929b8fd1b46cce2a1
Jonathan Graf <grajo721@t-online.de>
2024-06-21 08:40:19 +0200

    .0.5 just for testing

--- lib/pages/selection_page.dart
+++ lib/pages/selection_page.dart
@@ -7,7 +7,7 @@ class SelectionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      persistentFooterButtons: [const Text("Theo, falls du das siehst, gib Bilder  |  v1.0.3")],
+      persistentFooterButtons: [const Text("v1.0.5")],
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: Center(
