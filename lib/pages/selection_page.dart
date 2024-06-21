a6338a05371278cfe2cdeae60533010b72abfeae
Jonathan Graf <grajo721@t-online.de>
2024-06-21 10:22:07 +0200

    1.2.0 precaching

--- lib/pages/selection_page.dart
+++ lib/pages/selection_page.dart
@@ -7,7 +7,7 @@ class SelectionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      persistentFooterButtons: [const Text("v1.1.1")],
+      persistentFooterButtons: [const Text("v1.2.0")],
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: Center(
